dcfe02f66284be3495934e56b2a269e4f7d5c3ae
glide-the <2533736852@qq.com>
2024-07-17 15:04:06 +0800

    utf08

--- tests/unit_tests/test_code_interpreter.py
+++ tests/unit_tests/test_code_interpreter.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 import sys
-
 import pytest
-
 from langchain_glm.agent_toolkits.all_tools.code_interpreter_tool import (
     CodeInterpreterAllToolExecutor,
 )
 
+
 # 3.9以上才能运行
-
-
 @pytest.mark.skipif(sys.version_info < (3, 9), reason="Requires Python 3.9 or higher")
 def test_python_ast_interpreter():
     out = CodeInterpreterAllToolExecutor._python_ast_interpreter(
@@ -17,8 +14,8 @@ def test_python_ast_interpreter():
     )
     print(out.data)
     assert (
-        out.data
-        != """Access：code_interpreter,python_repl_ast, Message: print('Hello, World!')
+            out.data
+            != """Access：code_interpreter,python_repl_ast, Message: print('Hello, World!')
 Hello, World!
 """
     )
